f678a10de8ea068af1e649e004d5b269648fdf50
mahmoud <guebsimahmoud7@gmail.com>
2025-08-05 01:06:29 +0100

    first commit

---
+++ car.ts
@@ -0,0 +1,28 @@
+// Define the Vehicle interface
+interface Vehicle {
+    make: string;
+    model: string;
+    year: number;
+    start(): void;
+}
+
+// Implement the Car class
+class Car implements Vehicle {
+    make: string;
+    model: string;
+    year: number;
+
+    constructor(make: string, model: string, year: number) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    start(): void {
+        console.log("Car engine started");
+    }
+}
+
+// Create an instance and test
+const myCar = new Car("Toyota", "Corolla", 2022);
+myCar.start(); // Should log: Car engine started
